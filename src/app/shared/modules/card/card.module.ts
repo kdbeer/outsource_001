98e942e4707c6f2819d04609386e99e6b5046028
Kridsada Duangmala <kridsadacpe@gmail.com>
2018-05-01 00:46:42 +0700

    add card module

---
+++ src/app/shared/modules/card/card.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CardRoutingModule } from './card-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CardRoutingModule
+  ],
+  declarations: []
+})
+export class CardModule { }
